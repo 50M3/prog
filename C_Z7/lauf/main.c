611f174f81755227dfabc4ae4c1510bef76df1d1
50M3 <grafik.liehr@gmail.com>
2018-11-17 20:52:23 +0100

    Comments added

--- C_Z7/lauf/main.c
+++ C_Z7/lauf/main.c
@@ -1,34 +1,67 @@
+//  ***********************************************************************************************************
+//  Name            : main.c
+//  Autor           : Florian Liehr
+//  Datum           : 17.11.2018
+//  Version         : 1.2
+//  Beschreibung    : Run-Length Encoding
+//  ***********************************************************************************************************
+
+// INCLUDE BLOCK
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// PREDEFINE BLOCK
 #define MAX_VALUE 255
 void compress(char *string, int length);
 int errorcheck(int length);
 
+/**********************************************
+ *                                            *
+ *             MAIN FUNCTION                  *
+ * Erstellt den String und gibt diesen an den *
+ * Compression Algorithmus weiter.            *
+ *                                            *
+ **********************************************/
 int main() {
+
+    // CREATE ARRAY
     char string[MAX_VALUE];
+    // FILL ARRAY WITH STRING
     printf("String:\n");
     scanf("%[^\n]", string);
-    getchar();
+
+    // LENGTH OF STRING BY strlen function
     int length = strlen(string);
+
+    // ERRORCHECK IF INPUT IS LEGAL
     errorcheck(length);
 
+    // PRINTF BLOCK
     printf("Iput: %s\n", string);
     printf("Length: %d\n", length);
 
+    // STRING COMPRESSION
     compress(string, length);
 
     getchar();
     return 0;
 }
-
+/**********************************************
+ *                                            *
+ *           COMPRESS FUNCTION                *
+ * Erstellt durch einlesen des Strings und    *
+ * seiner Länge einen komprimierten String    *
+ * durch die Nutzung der Lauflängenkodierung  *
+ *                                            *
+ **********************************************/
 void compress(char *string, int length){
     int j = 0;
     int count = 0;
 
         while (j < length) {
 
-            while (string[j] == string[j + 1]) {
+            while (string[j] == string[j+1]) {
                 count++;
                 j++;
             }
@@ -41,7 +74,14 @@ void compress(char *string, int length){
             j++;
         }
 }
-
+/**********************************************
+ *                                            *
+ *           ERRORCHECK FUNCTION              *
+ * Es wird checkt ob die Länge des Strings    *
+ * größer der Größe des Arrays ist und ob     *
+ * eine leere Eingabe getätigt wurde.         *
+ *                                            *
+ **********************************************/
 int errorcheck(int length){
     if (length > MAX_VALUE || length == 0){
         printf("Wrong length! EXIT");

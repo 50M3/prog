d2dbefeec0b3ed919f7dfd2df13029f497adadc9
50M3 <grafik.liehr@gmail.com>
2018-11-17 12:04:26 +0100

    Small Bug Fix

--- C_Z7/lauf/cmake-build-debug/CMakeFiles/lauf.dir/DependInfo.cmake
+++ C_Z7/lauf/cmake-build-debug/CMakeFiles/lauf.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Admin/CLionProjects/lauf/main.c" "C:/Users/Admin/CLionProjects/lauf/cmake-build-debug/CMakeFiles/lauf.dir/main.c.obj"
+  "C:/Users/Florian Liehr/Documents/prog_git/C_Z7/lauf/main.c" "C:/Users/Florian Liehr/Documents/prog_git/C_Z7/lauf/cmake-build-debug/CMakeFiles/lauf.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- C_Z7/lauf/main.c
+++ C_Z7/lauf/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_VALUE 255
 void compress(char *string, int length);
+int errorcheck(int length);
 
 int main() {
-    char string[255];
+    char string[MAX_VALUE];
     printf("String:\n");
     scanf("%[^\n]", string);
     getchar();
     int length = strlen(string);
+    errorcheck(length);
 
     printf("Iput: %s\n", string);
     printf("Length: %d\n", length);
@@ -23,19 +26,25 @@ void compress(char *string, int length){
     int j = 0;
     int count = 0;
 
-    while(j < length){
+        while (j < length) {
 
-        while(string[j] == string[j+1]) {
-            count++;
+            while (string[j] == string[j + 1]) {
+                count++;
+                j++;
+            }
+            if (count < 1) {
+                printf("%c", string[j]);
+            } else if (count >= 1) {
+                printf("%c%d", string[j], count + 1);
+            }
+            count = 0;
             j++;
         }
-        if(count <= 1){
-            printf("%c", string[j]);
-        } else if(count > 1) {
-            printf("%c%d", string[j], count + 1);
-        }
-        count = 0;
-        j++;
-    }
+}
 
+int errorcheck(int length){
+    if (length > MAX_VALUE || length == 0){
+        printf("Wrong length! EXIT");
+        exit(-1);
+    }
 }
